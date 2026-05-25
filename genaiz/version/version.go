bb4cb7ba129055361106f88ae53402b8c029c7da
Guillaume Pelletier <guillaume.pelletier@genaiz.com>
2026-05-25 10:54:53 -0400

    Fixes 210 - Poll OIDC endpoint for validating a session - Promotes the SDK to version 0.4.X - Fixed filez.CloseSilently which was taking a ReadCloser when Closer is all that is needed - Refactors all the URL handling under the OIDC functionality with an typed Handler

--- genaiz/version/version.go
+++ genaiz/version/version.go
@@ -6,7 +6,7 @@ import (
 	"genaiz.com/genaiz/version/env"
 )
 
-var version = "0.3.10"
+var version = "0.4.0"
 
 func GetVersion() string {
 	return fmt.Sprintf("%s%s", version, env.GetVersionTag())
